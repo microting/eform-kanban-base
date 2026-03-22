dc6c44d0f3cd3bce99dfeeb17098df09dcb08cd6
René Schultz Madsen <rm@microting.dk>
2026-03-22 10:11:52 +0100

    Add KanbanClaims constants

---
+++ Microting.KanbanBase/Infrastructure/Const/KanbanClaims.cs
@@ -0,0 +1,14 @@
+namespace Microting.KanbanBase.Infrastructure.Const;
+
+public static class KanbanClaims
+{
+    public const string AccessKanbanPlugin = "access_kanban_plugin";
+    public const string GetBoards = "get_kanban_boards";
+    public const string CreateBoards = "create_kanban_boards";
+    public const string ManageBoards = "manage_kanban_boards";
+    public const string GetCards = "get_kanban_cards";
+    public const string ManageCards = "manage_kanban_cards";
+    public const string GetProjects = "get_kanban_projects";
+    public const string ManageProjects = "manage_kanban_projects";
+    public const string ViewCharts = "view_kanban_charts";
+}
